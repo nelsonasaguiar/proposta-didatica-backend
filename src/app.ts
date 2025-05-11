ee05374fbd6d56185fb68d7b82e3108d6e50a810
Nelson Aguiar <naguiar997@gmail.com>
2025-05-11 21:14:33 +0100

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express';
+import router from './routes/index'; // Import the router directly
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use the router as middleware
+app.use('/', router);
+
+const PORT: number = parseInt(process.env.PORT || '3000', 10);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Extract the version from package.json
+PACKAGE_VERSION=$(cat ./package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
+
+# Define the Docker image name
+IMAGE="propostadidatica-vm:${PACKAGE_VERSION}"
+
+# Build the Docker image
+docker build -t ${IMAGE} .
+
+# Stop and remove the old container
+docker stop proposta-didatica || true
+docker rm proposta-didatica || true
+
+# Run the new container
+docker run --restart=always -d -p 3032:3032 --name proposta-didatica ${IMAGE}
