581258d351047d32aef4901de27fe2c6016b5c29
Nelson Aguiar <naguiar997@gmail.com>
2025-05-11 21:17:08 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -11,5 +11,5 @@ app.use('/', router);
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}...`);
 });
