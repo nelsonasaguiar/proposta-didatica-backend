f4c497f8c76abd0e6061f5c7bae33d78ec243d9b
Nelson Aguiar <naguiar997@gmail.com>
2025-05-13 23:57:18 +0100

    updated .sh file?

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 # PACKAGE_VERSION=$(cat ./package.json | grep '"version"' | head -1 | awk -F: '{ print $2 }' | sed 's/[", ]//g')
 
 # Define the Docker image name
-IMAGE="propostadidatica-vm:latest"
+IMAGE="smart-car-api:latest"
 
 # Build the Docker image
 docker build -t ${IMAGE} .
 
 # Stop and remove the old container
-docker stop proposta-didatica || true
-docker rm proposta-didatica || true
+docker stop smart-car-api || true
+docker rm smart-car-api || true
 
 # Run the new container
-docker run --restart=always -d -p 3030:3000 --name proposta-didatica ${IMAGE}
+docker run --restart=always -d -p 3030:3000 --name smart-car-api ${IMAGE}
