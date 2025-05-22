57d70f7d2f2ecf4b00f98a4650cdcc985f4ba0c2
Nelson Aguiar <naguiar997@gmail.com>
2025-05-22 22:48:48 +0100

    updated callback url

--- src/helper/smartcar.helper.ts
+++ src/helper/smartcar.helper.ts
@@ -3,7 +3,7 @@ const smartcar = require('smartcar');
 const smartcarClient = new smartcar.AuthClient({
    clientId: "dc0f0278-80d5-42c2-aa43-d0dc0bab588b",
    clientSecret: "47af6451-4e18-4ff2-920d-771123499182",
-   redirectUri: 'https://smartcarapi.propostadidatica.pt/api/callback'
+   redirectUri: 'https://api.propostadidatica.pt/api/callback'
 });
 
 export default smartcarClient
